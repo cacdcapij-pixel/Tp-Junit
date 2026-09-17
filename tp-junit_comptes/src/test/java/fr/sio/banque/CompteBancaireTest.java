6c7f8a721cd02d517ae93e092aac6997f1bcbd21
charles covin <cacdcapij@gmail.com>
2026-09-17 10:52:55 +0200

    test: retrait, cas nominal, limite du decouvert et montant invalide (rouge)

--- tp-junit_comptes/src/test/java/fr/sio/banque/CompteBancaireTest.java
+++ tp-junit_comptes/src/test/java/fr/sio/banque/CompteBancaireTest.java
@@ -1,5 +1,6 @@
 package fr.sio.banque;
 import fr.sio.banque.exceptions.MontantInvalideException;
+import fr.sio.banque.exceptions.SoldeInsuffisantException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +25,40 @@ public class CompteBancaireTest {
         CompteBancaire compte = new CompteBancaire("FR7612345", "Charles");
 
         assertThrows(MontantInvalideException.class, () -> compte.deposer(0));
+    }
+
+    @Test
+    void retirer_diminue_le_solde() {
+        CompteBancaire compte = new CompteBancaire("FR7612345", "Charles");
+        compte.deposer(200);
+
+        compte.retirer(50);
+
+        assertEquals(150, compte.getSolde(), 0.001);
+    }
+
+    @Test
+    void retirer_jusqu_au_decouvert_autorise_est_accepte() {
+        CompteBancaire compte = new CompteBancaire("FR7612345", "Charles", 200);
+
+        compte.retirer(200);
+
+        assertEquals(-200, compte.getSolde(), 0.001);
+    }
+
+    @Test
+    void retirer_un_centime_de_plus_que_le_decouvert_leve_une_exception() {
+        CompteBancaire compte = new CompteBancaire("FR7612345", "Charles", 200);
+
+        assertThrows(SoldeInsuffisantException.class, () -> compte.retirer(200.01));
+        assertEquals(0, compte.getSolde(), 0.001);
+    }
+
+    @Test
+    void retirer_montant_negatif_leve_une_exception() {
+        CompteBancaire compte = new CompteBancaire("FR7612345", "Charles");
+
+        assertThrows(MontantInvalideException.class, () -> compte.retirer(-50));
     }
 }
 
